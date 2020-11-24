dcd6c41b536503689bb2804c3e71a50ceb80a076
gk <girish5f9@sasi.ac.in>
2020-11-24 22:42:16 +0530

    Updates

---
+++ precache-manifest.0013f05fdaebee10d88ae47b4d490259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2899e91a697f04787b2078812d5b1e",
+    "url": "/girishkumar/index.html"
+  },
+  {
+    "revision": "5fa4e28404c196ecfa5b",
+    "url": "/girishkumar/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "dfcefc5145d015e1ce87",
+    "url": "/girishkumar/static/js/2.5937f0cf.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/girishkumar/static/js/2.5937f0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa4e28404c196ecfa5b",
+    "url": "/girishkumar/static/js/main.2769bffb.chunk.js"
+  },
+  {
+    "revision": "2d66c57e0d9c2bb292f4",
+    "url": "/girishkumar/static/js/runtime-main.cf68a450.js"
+  },
+  {
+    "revision": "5bcfe75c3dfd4af822bf520efa193cb1",
+    "url": "/girishkumar/static/media/cartoon.5bcfe75c.jpg"
+  }
+]);
